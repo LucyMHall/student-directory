6f096b44d98589dfae70c01e80f436f518ffdcc9
Lucy Hall <lucymhall1995@gmail.com>
2019-05-10 15:55:48 +0000

    Transform code into methods

--- directory.rb
+++ directory.rb
@@ -14,12 +14,23 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
   ]
-  
-#print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |student| puts student }
+ 
+# Put actions in methods
 
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
+def print(names)
+  names.each { |name| puts name }
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# Nothing happens until we call the methods, remember to put in correct parameters
+print_header
+print(students)
+print_footer(students)
